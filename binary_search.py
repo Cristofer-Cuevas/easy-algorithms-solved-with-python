12a106db35cad73e23defe9c3ac30bde71ec2487
programmerXS <cristoferperezmejia80@gmail.com>
2022-03-24 18:20:46 -0400

    linear search algorithm finished

--- binary_search.py
+++ binary_search.py
@@ -7,17 +7,33 @@ def binary_search(numbers, target):
 	while first <= last:
 		mid_point = (first + last) // 2
 		if numbers[mid_point] == target: 
-			return mid_point
+			return print('Was value found?:', True)
 		elif numbers[mid_point] < target:
 			first = mid_point - 1
 		else:
-			last = mid_point + 1 
+			last = mid_point + 1
 
 
-numbers = [x for x in range(1, 10)]
+numbers = [x for x in range(1, 11)]
 
-print(numbers)
+# print(numbers)
 
-print(binary_search(numbers, 5))
+binary_search(numbers, 5)
 
 
+# Recursive implementation
+def recursive_binary_search(numbers, target):
+	# O(1)
+	if len(numbers) == 0:	return print('Was recursive binary search result value found?:', False)
+
+	mid_point = (len(numbers)) // 2
+	# O(log n)
+	if numbers[mid_point] == target:
+		return print('Was recursive binary search result value found?:', True)
+	else: 
+		if numbers[mid_point] < target:
+			recursive_binary_search(numbers[mid_point+1:], target)
+		elif numbers[mid_point] > target: 
+			recursive_binary_search(numbers[:mid_point], target)
+
+recursive_binary_search(numbers, 11)
